7e027af513f1080c2069f6603db540e29cab0085
Estéban <esteban.soubiran@insa-cvl.fr>
2023-11-29 10:55:49 +0000

    feat: add bug

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 const { data } = await useFetch('/api/data.json')
 
+const item = data.value?.data.foo.length
+
 if (!data.value) {
   throw createError({
     statusCode: 404,
