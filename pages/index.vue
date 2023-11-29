b1889a637454d8adfcdf22dfc3900e3fb446f5fe
Estéban <esteban.soubiran@insa-cvl.fr>
2023-11-29 11:01:03 +0000

    fix: script

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
 const { data } = await useFetch('/api/data.json')
 
-const item = data.value?.data.foo.length
-
 if (!data.value) {
   throw createError({
     statusCode: 404,
